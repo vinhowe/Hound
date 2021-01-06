8691b55bd4366e7300facb17283319a18bd07dca
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-01-05 22:51:08 -0500

    add support for glowing chests

--- src/main/java/io/github/midnightfury/hound/Hound.kt
+++ src/main/java/io/github/midnightfury/hound/Hound.kt
@@ -1,10 +1,16 @@
 package io.github.midnightfury.hound;
 
+import com.comphenix.protocol.ProtocolLibrary
+import com.comphenix.protocol.ProtocolManager
 import org.bukkit.plugin.java.JavaPlugin;
 
 class Hound: JavaPlugin() {
+    lateinit var protocolManager: ProtocolManager
+
     override fun onEnable() {
         // Plugin startup logic
+        protocolManager = ProtocolLibrary.getProtocolManager();
+        getCommand("buttonglow")?.setExecutor(SearchCommand(this))
 
     }
 
